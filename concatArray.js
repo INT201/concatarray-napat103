78e7b9af902f601a5369eca162f55c30d01c72d0
napat103 <naphat.watt@kmutt.ac.th>
2022-09-08 18:51:47 +0700

    finish

--- concatArray.js
+++ concatArray.js
@@ -1,7 +1,16 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
-  console.log("Hello")
+  if((array1 === null || array1 === undefined) && (array2 === null|| array2 === undefined)) {
+    return undefined
+  } else if(array1 === [] && array2 === []) {
+    return []
+  } else if(array2 === null || array2 === undefined || array2 === []) {
+    return array1
+  } else if(array1 === null || array1 ===undefined || array1 === []) {
+    return array2
+  } else return array1.concat(array2)
 }
+
 module.exports = concatArray
+
